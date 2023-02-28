7f81009d6e402cf510a4e59bead53729ba8e04b2
yzp <2306807556@qq.com>
2023-02-28 19:20:35 +0800

    icWht

--- src/main/java/com/example/easyexcel/excel/importer/listener/IcWhtReceiptListener.java
+++ src/main/java/com/example/easyexcel/excel/importer/listener/IcWhtReceiptListener.java
@@ -3,6 +3,7 @@ package com.example.easyexcel.excel.importer.listener;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.example.easyexcel.excel.importer.entity.IcWhtReceiptData;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 描述信息
@@ -11,14 +12,15 @@ import com.example.easyexcel.excel.importer.entity.IcWhtReceiptData;
  * @version 1
  * @since 2023-02-25 21:11
  */
+@Slf4j
 public class IcWhtReceiptListener extends AnalysisEventListener<IcWhtReceiptData> {
     @Override
     public void invoke(IcWhtReceiptData data, AnalysisContext context) {
-
+        log.info("解析到一条数据：{}", data);
     }
 
     @Override
     public void doAfterAllAnalysed(AnalysisContext context) {
-
+        log.info("解析全部数据结束！");
     }
 }
