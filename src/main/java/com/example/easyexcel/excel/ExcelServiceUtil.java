4268e28f3c67e856050d9cbfdb4a7894b6cb58d8
yzp <2306807556@qq.com>
2023-03-01 13:11:02 +0800

    工具类添加私有构造方法以隐藏隐式公共构造方法

--- src/main/java/com/example/easyexcel/excel/ExcelServiceUtil.java
+++ src/main/java/com/example/easyexcel/excel/ExcelServiceUtil.java
@@ -13,6 +13,9 @@ import java.io.InputStream;
  * @since 2023-02-25 17:29
  */
 public class ExcelServiceUtil {
+    private ExcelServiceUtil() {
+    }
+
     public static <T> void read(String fileName, Class<T> clz, AnalysisEventListener<T> readListener) {
         EasyExcel.read(fileName, clz, readListener).sheet().doRead();
     }
